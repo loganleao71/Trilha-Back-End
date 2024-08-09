b87e9045821552cdb5a1c6d0931691e6d862ebef
loganleao71 <loganleao71@gmail.com>
2024-08-09 19:41:24 -0300

    first commit

---
+++ node_mudules/index.js
@@ -0,0 +1,55 @@
+const express = reqire('express');
+
+const server = express();
+
+server.use(express.json());
+
+const cursos = ['FullStack Masters',' Desenvolvimento de Games', 'Viver de Youtube'];
+
+server.get('/cursos/:index', (req, res) =>{
+    const { index } = req.params;
+
+    return res.json(cursos[index]);
+});
+
+
+server.get('/cursos', (req, res) => {
+    return res.json(cursos);
+});
+
+
+server.post('/cursos', (req, res) => {
+    const { name } = req.body;
+    cursos.push(name);
+
+    return res.json(cursos);
+    
+});
+
+
+server.put('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    cursos [index] = name;
+
+    return res.json(cursos);
+    
+});
+
+server.delete('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+
+    cursos.splice(index, 1);
+    return res.json({ mensage: "O curso foi deletado"});
+
+
+});
+
+
+
+
+
+
+
+server.listen(3000);
